691c18eb7911e3d52320b770c23090eca8e8132e
alxkho <alexa.khokhlova13@gmail.com>
2022-08-02 23:33:07 +0300

    add feature

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            Console.Write("Enter your name ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
             Console.ReadLine();
         }
     }
