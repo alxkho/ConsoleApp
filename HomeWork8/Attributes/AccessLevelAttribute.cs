5ca5b7c427c08b0f62254c8a236bfc259f9bf002
alxkho <alexa.khokhlova13@gmail.com>
2022-10-10 23:23:53 +0300

    add attributes

---
+++ HomeWork8/Attributes/AccessLevelAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeWork8.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    public class AccessLevelAttribute : Attribute
+    {
+        public AccesLevelType Role { get; set; }
+
+        public AccessLevelAttribute(AccesLevelType data)
+        {
+            Role = data;
+        }
+    }
+}
---
+++ HomeWork8/Attributes/AttributeProgram.cs
@@ -0,0 +1,30 @@
+﻿namespace HomeWork8.Attributes
+{
+    public class AttributeProgram
+    {
+        public static void Start()
+        {
+            Director director = new Director();
+            Manager manager = new Manager();
+            Worker worker = new Worker();
+
+            GetAccess(director, AccesLevelType.High);
+            GetAccess(director, AccesLevelType.Low);
+            GetAccess(manager, AccesLevelType.High);
+            GetAccess(worker, AccesLevelType.Average);
+            GetAccess(worker, AccesLevelType.Low);
+        }
+
+        static void GetAccess(Employee employee, AccesLevelType acces)
+        {
+            var type = employee.GetType();
+            var attr = (AccessLevelAttribute)Attribute.GetCustomAttribute(type, typeof(AccessLevelAttribute));
+            var role = attr.Role;
+
+            if (role <= acces)
+                Console.WriteLine("Доступ к данным разрешен");
+            else
+                Console.WriteLine("Доступ к данным запрещен");
+        }
+    }
+}
