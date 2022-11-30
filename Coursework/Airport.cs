c09a8fda02c8631465febaa564299df02f26ee48
khokhlova_a <khokhlova_a@ipps.by>
2022-11-30 16:38:32 +0300

    add coursework

---
+++ Coursework/Airport.cs
@@ -0,0 +1,14 @@
+﻿namespace Coursework
+{
+    public class Airport : IdName
+    {
+        string Country { get; set; }
+        string City { get; set; }
+        string Size { get; set; }
+        public Airport(int id, string name, string city, string country) : base(id, name)
+        {
+            Country = country;
+            City = city;
+        }
+    }
+}
