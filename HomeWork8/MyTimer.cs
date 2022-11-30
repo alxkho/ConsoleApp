91df6b15f398ade4c8b046ee629f09c7c291b2ca
khokhlova_a <khokhlova_a@ipps.by>
2022-11-30 16:10:23 +0300

    add timer

---
+++ HomeWork8/MyTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    public class MyTimer
+    {
+        public void Start(int interval)
+        {
+            // устанавливаем метод обратного вызова
+            TimerCallback tm = new TimerCallback(
+               (r) => Console.WriteLine($"{(int)r}")
+            );
+            for (int i = 0; i < interval; i++)
+            {
+                Timer timer = new Timer(tm, i, 0, 2000);
+            }
+            return;
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork8/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTimer timer = new MyTimer();
+            timer.Start(10);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
